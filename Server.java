5aef1a8187d7ff95a435e1236dbf6319a5e0af24
hscasn <henriquesc@gmail.com>
2022-05-28 16:17:21 +0000

    Cleaning up Server file

--- Server.java
+++ Server.java
@@ -1,31 +1,22 @@
 package com.company;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Server {
-    static Gson gson = new Gson();
-    static Store listItems;
+    private Gson gson = new Gson();
+    private Store listItems;
+
     public Server(Store store) throws Exception {
         listItems = store;
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 420), 0);
-        server.createContext("/list", new ListHandler());
-        server.createContext("/add", new AddHandler());
-        server.createContext("/delete", new DeleteHandler());
-        server.setExecutor(null);
-        server.start();
-    }
 
-    static class ListHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
+        server.createContext("/list", (HttpExchange t) -> {
             if (!t.getRequestMethod().equals("GET")) {
                 t.sendResponseHeaders(405, 0);
                 t.close();
@@ -40,12 +31,9 @@ public class Server {
             OutputStream os = t.getResponseBody();
             os.write(jsonResponse.getBytes());
             os.close();
-        }
-    }
+        });
 
-    static class AddHandler implements HttpHandler{
-        @Override
-        public void handle(HttpExchange t) throws IOException{
+        server.createContext("/add", (HttpExchange t) -> {
             Task task = gson.fromJson(new InputStreamReader(t.getRequestBody()), Task.class);
             if (!t.getRequestMethod().equals("POST")) {
                 t.sendResponseHeaders(405, 0);
@@ -60,12 +48,9 @@ public class Server {
             listItems.addItem(task.name);
             t.sendResponseHeaders(200, 0);
             t.close();
-        }
-    }
+        });
 
-    static class DeleteHandler implements HttpHandler{
-        @Override
-        public void handle(HttpExchange t) throws IOException{
+        server.createContext("/delete", (HttpExchange t) -> {
             Task task = gson.fromJson(new InputStreamReader(t.getRequestBody()), Task.class);
             if (!t.getRequestMethod().equals("DELETE")) {
                 t.sendResponseHeaders(405, 0);
@@ -80,6 +65,9 @@ public class Server {
             listItems.removeItem(task.name);
             t.sendResponseHeaders(200, 0);
             t.close();
-        }
+        });
+
+        server.setExecutor(null);
+        server.start();
     }
 }
