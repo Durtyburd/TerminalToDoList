268de7fa271a45043e0d9f6cd7c02cf80a281bfc
Seth Perritt <sethperritt@yahoo.com>
2022-06-22 19:11:27 -0700

    connected server to js front

---
+++ src/JSToDoListFront/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  mode: "development",
+};
---
+++ src/main/java/com/company/FileTasks.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class FileTasks {
+
+    public static ArrayList<String> extractFile(String filePath) throws IOException {
+        Stream<Path> paths = Files.walk(Paths.get(filePath));
+        ArrayList<String> list = new ArrayList<>();
+        paths.forEach(y -> list.add(String.valueOf(y)));
+        return list;
+    };
+
+    public static String readFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        String list = "";
+        Scanner myReader = new Scanner(file);
+        System.out.println("Starting loop");
+        while (myReader.hasNextLine()) {
+            list = list + myReader.nextLine() + "\n";
+        }
+        System.out.println(list);
+        return list;
+    };
+}
